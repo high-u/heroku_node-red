c2386506be856a29c6ddb9384c3ae2d2b8500a13
high-u <zen.crazyd@gmail.com>
2018-04-16 21:27:01 +0900

    add Node-RED app

---
+++ app.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+// Expressアプリケーションの生成
+var app = express();
+
+// 静的コンテンツのルートを追加
+app.use("/",express.static("public"));
+
+// サーバの生成
+var server = http.createServer(app);
+
+// 設定オブジェクトの生成 - 他のオプションについてはデフォルトの 'settings.js' ファイルを参照してください
+var settings = {
+  uiPort: 1882,
+  httpAdminRoot:"/",
+  httpNodeRoot: "/api",
+  userDir:"./node-red_userdir",
+  functionGlobalContext: { }    // グローバルコンテキストを有効化
+};
+
+// サーバと設定とランタイムの初期化
+RED.init(server,settings);
+
+// エディタUIのルートを '/red' に指定
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// HTTP node UIのルートを '/api' に指定
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(1882);
+
+// ランタイム起動
+RED.start();
